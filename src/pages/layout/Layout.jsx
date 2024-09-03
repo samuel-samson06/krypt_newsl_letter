998aba2ed6ab62a1355f0fa8f34dfd286bc53ed3
samuel-samson06 <a.samuelsamson@gmail.com>
2024-09-03 14:32:18 +0100

    ..

--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -2,7 +2,6 @@ import React, { useContext } from 'react'
 import Header from './Header'
 import { Outlet } from 'react-router-dom'
 import { MyContext } from '../../context/Context'
-import ReactPlayer from 'react-player/lazy'
 import video from "../../assets/bg_video.mp4"
 
 function Layout() {
@@ -12,11 +11,7 @@ function Layout() {
         <div className={` ${darkMode? "bg-[#393a3a]" : "bg-[#ebf7f8]"} h-screen  flex flex-col min-h-screen`}>
             <Header/>
             <div className={` flex-grow w-full  relative `}>
-                {/* <video src={video}  className=' w-full h-full object-cover absolute top-0 left-0' playsInline loop autoPlay muted ></video> */}
-                  
-                  <div className=' w-full h-full object-cover absolute top-0 left-0 '>
-                  <ReactPlayer url={video} loop={true} playing={true} height={"100%"} width={"100%"}/>
-                  </div>
+                <video src={video}  className=' w-full h-full object-cover absolute top-0 left-0' playsInline loop autoPlay muted ></video>
                 <main className={` relative bg-gradient-to-br from-[#12121282] to-[#2b151598] bg-opacity-80 z-10  md:flex md:items-center rounded-xl mx-1 font-medium md:px-4 my-6 md:my-1 pb-10 text-yellow-500 `}>
                   <Outlet/>
                 </main>
@@ -31,9 +26,3 @@ export default Layout
 
 // ${darkMode?" from-[#81b77b] to-[#54b150]":"from-[#b1faaa] to-[#79f872]"} 
 // bg-gray-200
-
-//   {/* <video src={video}  className=' w-full h-full object-cover absolute top-0 left-0' playsInline loop autoPlay muted ></video> */}
-                  
-{/* <div className=' w-full h-full object-cover absolute top-0 left-0 '>
-<ReactPlayer url={video} loop={true} playing={true} height={"100%"} width={"100%"}/>
-</div> */}
